e3c555f9c87540ee860d35a7b68780fb98baf26d
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-04-10 00:35:30 +0600

    added the form submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
 
 function App() {
 
@@ -8,8 +9,8 @@ function App() {
   return (
     <>
      
-      <h1>Vite + React</h1>
-   
+      <h1 className='font-bold text-3xl mb-5'>Vite + React</h1>
+      <SimpleForm></SimpleForm>
       
     </>
   )
